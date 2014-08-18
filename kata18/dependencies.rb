7e582157bbf5d69c67e2d60e29c7e1a4449252fd
Kleber Shimabuku <klebershimabuku@gmail.com>
2014-08-18 15:58:19 -0300

    Initial Commit

---
+++ kata18/dependencies.rb
@@ -0,0 +1,7 @@
+class Dependencies
+  def add_direct(obj, dependencies)
+  end
+
+  def dependencies_for(obj)
+  end
+end
---
+++ spec/dependencies_spec.rb
@@ -0,0 +1,20 @@
+require './kata18/dependencies'
+
+describe Dependencies do
+  it 'basic' do
+    dep = Dependencies.new
+    dep.add_direct('A', %w{ B C } )
+    dep.add_direct('B', %w{ C E } )
+    dep.add_direct('C', %w{ G   } )
+    dep.add_direct('D', %w{ A F } )
+    dep.add_direct('E', %w{ F   } )
+    dep.add_direct('F', %w{ H   } )
+
+    expect(dep.dependencies_for('A').to eq(%w{ B C E F G H }))
+    expect(dep.dependencies_for('B').to eq(%w{ C E F G H }))
+    expect(dep.dependencies_for('C').to eq(%w{ G }))
+    expect(dep.dependencies_for('D').to eq(%w{ A B C E F G H }))
+    expect(dep.dependencies_for('E').to eq(%w{ F H }))
+    expect(dep.dependencies_for('F').to eq(%w{ H }))
+  end
+end
