b8e7b66f42ecebb7c4a413530f983fe72a6d8ca9
Kleber Shimabuku <klebershimabuku@gmail.com>
2014-08-19 16:00:11 -0300

    basic dependencies

--- kata18/dependencies.rb
+++ kata18/dependencies.rb
@@ -1,7 +1,33 @@
+require 'pry'
 class Dependencies
-  def add_direct(obj, dependencies)
+  def initialize
+    @items = {}
   end
 
-  def dependencies_for(obj)
+  def add_direct(element, dependencies)
+    @items[element] = dependencies
+  end
+
+  def dependencies_for(element)
+    direct_dependencies = dependencies(element)
+    sub_dependencies = []
+
+    direct_dependencies.each do |dep|
+      sub_dependencies << dependencies(dep)
+    end
+
+    [direct_dependencies + sub_dependencies].flatten.uniq.sort
+  end
+
+  protected
+  def dependencies(element)
+
+    list = @items.select { |k,_| k == element }.values.flatten
+    sub_deps = []
+    list.each do |item|
+      sub_deps << dependencies(item)
+    end
+
+    list + sub_deps
   end
 end
--- spec/dependencies_spec.rb
+++ spec/dependencies_spec.rb
@@ -10,11 +10,22 @@ describe Dependencies do
     dep.add_direct('E', %w{ F   } )
     dep.add_direct('F', %w{ H   } )
 
-    expect(dep.dependencies_for('A').to eq(%w{ B C E F G H }))
-    expect(dep.dependencies_for('B').to eq(%w{ C E F G H }))
-    expect(dep.dependencies_for('C').to eq(%w{ G }))
-    expect(dep.dependencies_for('D').to eq(%w{ A B C E F G H }))
-    expect(dep.dependencies_for('E').to eq(%w{ F H }))
-    expect(dep.dependencies_for('F').to eq(%w{ H }))
+    expect(dep.dependencies_for('A')).to eq(%w{ B C E F G H })
+    expect(dep.dependencies_for('B')).to eq(%w{ C E F G H })
+    expect(dep.dependencies_for('C')).to eq(%w{ G })
+    expect(dep.dependencies_for('D')).to eq(%w{ A B C E F G H })
+    expect(dep.dependencies_for('E')).to eq(%w{ F H })
+    expect(dep.dependencies_for('F')).to eq(%w{ H })
+  end
+
+  xit 'cyclic' do
+    dep = Dependencies.new
+    dep.add_direct('A', 'B')
+    dep.add_direct('B', 'C')
+    dep.add_direct('C', 'A')
+
+    expect(dep.dependencies_for('A')).to eq(%w{ B C })
+    expect(dep.dependencies_for('B')).to eq(%w{ A C })
+    expect(dep.dependencies_for('C')).to eq(%w{ A B })
   end
 end
